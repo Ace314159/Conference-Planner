959704f3fb454389af110f93f04f8998e14ea07f
Akash <akash.munagala@gmail.com>
2018-03-19 21:52:32 +0530

    Update utility for setting up breaks

--- setupBreaks.py
+++ setupBreaks.py
@@ -1,49 +1,50 @@
 import sys
+import json
+from datetime import datetime
 
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import auth
 
 schoolsID = "Schools"
 teachersID = "Teachers"
 breaksStartID = "breaksStart"
-breaksEndID = "breaksStart"
+breaksEndID = "breaksEnd"
+breaksGroupsID = "breaksGroups"
 
 if len(sys.argv) != 2:
-    raise ValueError("Need to specify school as first argument")
+	sys.argv.append("ISH")
+	# raise ValueError("Need to specify school as first argument")
 school = sys.argv[1]
 
 # Use the service account
-cred = credentials.Certificate('teacher_breaks_account.json')
+cred = credentials.Certificate('firebase_admin_service_account.json')
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 
 schoolRef = db.collection(schoolsID).document(school)
 teachersRef = schoolRef.collection(teachersID)
+
+teacherIDs = {teacher.to_dict()["email"]: teacher.id for teacher in teachersRef.get()}
+
 schoolRefData = schoolRef.get().to_dict()
-numGroups = len(schoolRefData[breaksStartID])
+schoolRefData = {}
+schoolRefData[breaksStartID] = {}
+schoolRefData[breaksEndID] = {}
+schoolRefData[breaksGroupsID] = {}
 
-# Create the groups
-teachers = list(teachersRef.get())
-groups = {}
-teachersLen = len(teachers)
-groupLen = teachersLen // numGroups
-rem = teachersLen % numGroups
+with open("breaks.json", "r") as f:
+	breaksData = json.load(f)
 
-i = 0
-count = 0
-while i < teachersLen:
-    if count < rem:
-        selected = teachers[i:i + groupLen + 1]
-        i += 1
-    else:
-        selected = teachers[i:i + groupLen]
-    i += groupLen
-    for teacher in selected:
-        groups[teacher.id] = count
-    count += 1
+for i, group in enumerate(breaksData):
+	schoolRefData[breaksStartID][str(i)] = [datetime.strptime(start, "%m/%d/%y %H:%M") for start in group["breakStarts"]]
+	schoolRefData[breaksEndID][str(i)] = [datetime.strptime(end, "%m/%d/%y %H:%M") for end in group["breakEnds"]]
+	for teacherID in group["teachers"]:
+		if teacherID in teacherIDs:
+			schoolRefData[breaksGroupsID][str(teacherIDs[teacherID])] = i
+		else:
+			print(teacherID)
 
-# Add the groups to the database
-schoolRefData["breaksGroups"] = groups
 schoolRef.set(schoolRefData)
